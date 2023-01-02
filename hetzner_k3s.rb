52b4fcb5853a648391017d1e781b3d07089e4307
Vito Botta <vito@botta.me>
2023-01-02 13:32:04 +0200

    Update hetzner_k3s.rb

--- hetzner_k3s.rb
+++ hetzner_k3s.rb
@@ -1,21 +1,21 @@
 class HetznerK3s < Formula
   desc 'This is a CLI tool to quickly create and manage Kubernetes clusters in Hetzner Cloud using the lightweight Kubernetes distribution k3s from Rancher.'
   homepage 'https://github.com/vitobotta/hetzner-k3s'
-  version '0.6.7'
+  version '0.6.8'
   license 'MIT'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/vitobotta/hetzner-k3s/releases/download/v0.6.7/hetzner-k3s-mac-arm64', using: :curl
-      sha256 '8d4939733d65acf76513a958f3beebee57a14ad64083c0632e2142d8e77a1e8c'
+      url 'https://github.com/vitobotta/hetzner-k3s/releases/download/v0.6.8/hetzner-k3s-mac-arm64', using: :curl
+      sha256 'cfcd1ebdeca3ce9deb08313f5a071ce8c0359ad10161117ea81c2a0d89d49949'
 
       def install
         bin.install 'hetzner-k3s-mac-arm64' => 'hetzner-k3s'
       end
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/vitobotta/hetzner-k3s/releases/download/v0.6.7/hetzner-k3s-mac-amd64', using: :curl
-      sha256 '602ca2ac2d2fe6f77a07b0bb72764740465c52952eb5bcc7b435a7092e4f5c2e'
+      url 'https://github.com/vitobotta/hetzner-k3s/releases/download/v0.6.8/hetzner-k3s-mac-amd64', using: :curl
+      sha256 'c7db18658b58303640896509b2a60a5082770c89fe5627bae87804186ff3bbbb'
 
       def install
         bin.install 'hetzner-k3s-mac-amd64' => 'hetzner-k3s'
@@ -24,8 +24,8 @@ class HetznerK3s < Formula
   end
 
   on_linux do
-    url 'https://github.com/vitobotta/hetzner-k3s/releases/download/v0.6.7/hetzner-k3s-linux-x86_64', using: :curl
-    sha256 '1fc794e30cc04427ee5fecd3139e5165a737be1a5a02eb18d789f3b4ff488998'
+    url 'https://github.com/vitobotta/hetzner-k3s/releases/download/v0.6.8/hetzner-k3s-linux-x86_64', using: :curl
+    sha256 '8ce223c1cb674dda4433f156af8c9b21c2777619b8e5531de42d51758afba6e0'
 
     def install
       bin.install 'hetzner-k3s-linux-x86_64' => 'hetzner-k3s'
