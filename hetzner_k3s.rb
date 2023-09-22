533c60bceaf4cdc9ff7da630e8d45bdf0d6306ff
Vito Botta <vito@botta.me>
2023-09-22 21:28:34 +0300

    Fix links

--- hetzner_k3s.rb
+++ hetzner_k3s.rb
@@ -14,7 +14,7 @@ class HetznerK3s < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/vitobotta/hetzner-k3s/releases/download/v1.1.4/hetzner-k3s-mac-arm64', using: :curl
+      url 'https://github.com/vitobotta/hetzner-k3s/releases/download/v1.1.4/hetzner-k3s-macos-arm64', using: :curl
       sha256 '456bd88efbc42a44b44e431b1351d187555d08ddaebe4af2be7009624eb3d2c4'
 
       def install
@@ -22,7 +22,7 @@ class HetznerK3s < Formula
       end
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/vitobotta/hetzner-k3s/releases/download/v1.1.4/hetzner-k3s-mac-amd64', using: :curl
+      url 'https://github.com/vitobotta/hetzner-k3s/releases/download/v1.1.4/hetzner-k3s-macos-amd64', using: :curl
       sha256 '892fd881e5c41a416c1e6cb8c313e919b28bea0e6405b627d534aa3873cc3611'
 
       def install
